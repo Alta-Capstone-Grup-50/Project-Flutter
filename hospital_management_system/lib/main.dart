b6f7604cdcae93e41b33e15162a82d29a1b79d82
Naufal Ulwan <75620422+opangpoo123@users.noreply.github.com>
2022-05-30 20:50:48 +0700

    ensure initialized

--- hospital_management_system/lib/main.dart
+++ hospital_management_system/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
